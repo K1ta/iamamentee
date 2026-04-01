ee79a24ef750b1315eab8fcdf0810869500f9d9d
kobalt <kobalt31@mail.ru>
2026-04-01 17:10:04 +0200

    add read index for products

---
+++ backend/products/migrations/20260401142035_products_index.sql
@@ -0,0 +1,4 @@
+-- +goose Up
+CREATE INDEX CONCURRENTLY IF NOT EXISTS products_read_idx ON products (id) INCLUDE (user_id, name, price);
+-- +goose Down
+DROP INDEX CONCURRENTLY IF EXISTS products_read_idx;
