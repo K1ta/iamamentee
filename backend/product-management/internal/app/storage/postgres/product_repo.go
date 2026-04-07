6fac8cde36021721f4e5afc8a22965a60eccfabf
kobalt <kobalt31@mail.ru>
2026-04-07 15:26:13 +0200

    WIP merge repos in one file

--- backend/product-management/internal/app/storage/postgres/product_repo.go
+++ backend/product-management/internal/app/storage/postgres/product_repo.go
@@ -3,8 +3,12 @@ package postgres
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
+	"log"
 	"product-management/internal/app/models"
+	"product-management/internal/pkg/sharding"
+	"strconv"
 )
 
 type ProductRepository struct {
@@ -54,4 +58,78 @@ func (r *ProductRepository) List(ctx context.Context, userID int64) ([]models.Pr
 		return nil, fmt.Errorf("rows: %w", err)
 	}
 	return res, nil
+}
+
+// ShardedProductRepository репозиторий для работы с шардами
+type ShardedProductRepository struct {
+	shards sharding.Shards[*ProductRepository]
+}
+
+func NewShardedProductRepository(shards sharding.Shards[*ProductRepository]) (*ShardedProductRepository, error) {
+	if len(shards) == 0 {
+		return nil, errors.New("no shards")
+	}
+	return &ShardedProductRepository{shards: shards}, nil
+}
+
+func (r *ShardedProductRepository) Create(ctx context.Context, product *models.Product) error {
+	_, repo := r.shards.Get(strconv.FormatInt(product.UserID, 10))
+	return repo.Create(ctx, product)
+}
+
+func (r *ShardedProductRepository) GetByID(ctx context.Context, id, userID int64) (*models.Product, error) {
+	_, repo := r.shards.Get(strconv.FormatInt(userID, 10))
+	return repo.GetByID(ctx, id, userID)
+}
+
+func (r *ShardedProductRepository) List(ctx context.Context, userID int64) ([]models.Product, error) {
+	_, repo := r.shards.Get(strconv.FormatInt(userID, 10))
+	return repo.List(ctx, userID)
+}
+
+// MigratingProductRepository репозиторий для работы с шардами во время миграции. WRITE в новые шарды, READ из новых с fallback к старым
+type MigratingProductRepository struct {
+	repo           *ShardedProductRepository
+	prevShardsRepo *ShardedProductRepository
+}
+
+func NewMigratingProductRepository(repo, prevShardsRepo *ShardedProductRepository) *MigratingProductRepository {
+	return &MigratingProductRepository{repo: repo, prevShardsRepo: prevShardsRepo}
+}
+
+func (r *MigratingProductRepository) Create(ctx context.Context, product *models.Product) error {
+	return r.repo.Create(ctx, product)
+}
+
+func (r *MigratingProductRepository) GetByID(ctx context.Context, id, userID int64) (*models.Product, error) {
+	product, err := r.repo.GetByID(ctx, id, userID)
+	if err == nil {
+		return product, nil
+	}
+	if !errors.Is(err, sql.ErrNoRows) {
+		return nil, fmt.Errorf("failed to get product %d from current shards: %w", id, err)
+	}
+	log.Printf("trying to get product %d from prev shards", id)
+	product, err = r.prevShardsRepo.GetByID(ctx, id, userID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get product %d from prev shards: %w", id, err)
+	}
+	return product, nil
+}
+
+func (r *MigratingProductRepository) List(ctx context.Context, userID int64) ([]models.Product, error) {
+	products, err := r.repo.List(ctx, userID)
+	if err == nil && len(products) > 0 {
+		return products, nil
+	}
+	if err != nil {
+		// логируем ошибку, но все равно пробуем сходить в старые шарды
+		log.Printf("failed to get products for user %d from current shards: %v", userID, err)
+	}
+	log.Printf("trying to get producers for usr %d from prev shards", userID)
+	products, err = r.prevShardsRepo.List(ctx, userID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get products for user %d from prev shards: %w", userID, err)
+	}
+	return products, nil
 }
